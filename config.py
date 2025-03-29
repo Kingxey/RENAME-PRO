5ca3a8c853b819cb96ebf56c98ddd048441fa058
Noob Dev <kingceylegend@gmail.com>
2025-03-29 16:57:45 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7561380664:AAFCKW5FbH451Ke82hxGMbYn-tCTjljWmIM")
+API_ID = int(os.environ.get("API_ID", "24817837"))
+API_HASH = os.environ.get("API_HASH", "acd9f0cc6beb08ce59383cf250052686")
+OWNER = int(os.environ.get("OWNER", "7428552084"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "renamer_4go_o_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "botzflix")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002376378205"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
